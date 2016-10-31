683596b3a90500f1fee018c159ddcdbb6ee0b1bb
Carlos Coves Prieto <escorponox@gmail.com>
2016-10-31 12:20:31 +0100

    responsive version

---
+++ scripts/coves-select.js
@@ -0,0 +1,1 @@
+!function(c){var e=c.querySelectorAll(".coves-form__select");[].forEach.call(e,function(c){c.style.color="#00c7ff",c.addEventListener("change",function(){""===c.value?c.style.color="#00c7ff":c.style.color="#000000"})})}(document);
